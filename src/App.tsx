01f84ec9c61b96112d4e5bd3a43985a3a9875d8e
Seraj Haqiqi <haqiqi.seraj@gmail.com>
2024-01-13 21:32:34 +0330

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const ref = useRef<HTMLInputElement>(null);
+  const pointerScroll = (elem: HTMLInputElement) => {
+
+    let isDrag = false;
+    
+    const dragStart = () => isDrag = true;
+    const dragEnd = () => isDrag = false;
+    const drag = (ev) => isDrag && (elem.scrollLeft -= ev.movementX);
+    
+    elem.addEventListener("pointerdown", dragStart);
+    addEventListener("pointerup", dragEnd);
+    addEventListener("pointermove", drag);
+  };
+  useEffect(()=>{
+    ref.current && pointerScroll(ref.current)
+  },[ref])
 
   return (
-    <>
-      <div>Container</div>
-      <div className="flex">
+    <div className="flex flex-col items-start select-none">
+      <div className="text-sm">Container</div>
+      <div
+        ref={ref}
+        className="flex gap-3 items-center w-200 bg-amber overflow-auto"
+        style={{touchAction: 'none'}}
+      >
         {new Array(20).fill(0).map((_, index) => (
-          <div key={index} className="w-50 h-50 flex flex-col">
-            <div className="w-20 h-20 bg-blue">Title of {index}</div>
+          <div
+            key={index}
+            className="w-35 h-35 flex flex-grow-0 flex-shrink-0 flex-col border-1 border-solid justify-center items-center bg-black"
+          >
             <div>Title of {index}</div>
-            {index}
+            <div className="w-20 h-20 bg-blue text-sm flex items-center justify-around">
+              Title of {index}
+            </div>
+            <div className="justify-center text-sm">description</div>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
